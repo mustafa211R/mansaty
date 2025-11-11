deaa404d9431197d871a4f20c90128a1a529d9ee
Mustafa Rahim Fandi <97108737+mustafa211R@users.noreply.github.com>
2025-11-11 20:07:07 +0300

    Update teachers.js

--- exams/2/Computer/teachers.js
+++ exams/2/Computer/teachers.js
@@ -1,23 +1,11 @@
 const teachers = [
   {
-    "name": "أ. علي أحمد",
-    "specialty": "لغة عربية",
-    "school": "مدرسة النخبة",
+    "name": "ا.مصطفى رحيم فندي",
+    "specialty": "حاسبات",
+    "school": "مدرسة ذي النفس الزكية",
     "image": "images_teachers/mustafa.jpg",
     "link": "teachers/Mustafa/questions.html"
-  },
-  {
-    "name": "أ. مريم خالد",
-    "specialty": "لغة عربية",
-    "school": "ثانوية المستقبل",
-    "image": "images_teachers/mariam_khaled.jpg",
-    "link": "teachers/mariam_khaled/questions.html"
-  },
-  {
-    "name": "أ. سامي يوسف",
-    "specialty": "لغة عربية",
-    "school": "إعدادية الإبداع",
-    "image": "images_teachers/sami_youssef.jpg",
-    "link": "teachers/sami_youssef/questions.html"
   }
+ 
 ];
+
