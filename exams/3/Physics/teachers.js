136ad97c861a45478c5f053382c0e08b7235dda1
Mustafa Rahim Fandi <97108737+mustafa211R@users.noreply.github.com>
2026-09-18 09:45:03 +0300

    Delete exams directory

--- exams/3/Physics/teachers.js
+++
@@ -1,9 +0,0 @@
-const teachers = [
-{
-  "name": "باسم عجيل",
-  "specialty": "فيزياء",
-  "school": "ذي النفس الزكية",
-  "image": "images_teachers/باسم_عجيل.jpeg",
-  "link": "teachers/باسم_عجيل/questions.html"
-}
-];
